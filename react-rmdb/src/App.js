9506753068e8d7b9d87169cb076687d7fe0a1713
Eric Nganga <ericmainaedits@gmail.com>
2021-08-21 18:05:34 -0500

    created Header,Home components

--- react-rmdb/src/App.js
+++ react-rmdb/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
+//Components
+import Header from './components/Header';
+import Home from './components/Home';
+
+//styles
+import { GlobalStyle } from './GlobalStyle';
+
+
 function App() {
   return (
     <div className="App">
-      Start here.
+      <Header />
+      <Home />
+      <GlobalStyle />
     </div>
   );
-}
+} 
 
 export default App;
